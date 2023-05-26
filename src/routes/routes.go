b270b4bfd2adbb754f96de37d2cd60ae54731c2d
yusrililhm <yusrililham62@gmail.com>
2023-05-27 03:29:35 +0700

    update routes.go

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -1,12 +1,26 @@
 package routes
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/yusrililhm/go-bookshelf-api/src/config"
 	"github.com/yusrililhm/go-bookshelf-api/src/handler"
 )
 
 func Routes()  {
 	r := gin.Default()
+
+	// port
+
+	port := config.GetEnv("PORT")
+
+	// cors
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"https://search.opensuse.org/"},
+		AllowMethods: []string{"PUT", "POST", "GET", "DELETE"},
+		AllowHeaders: []string{"Content-Type", "application/json"},
+	}))
 
 	book := r.Group("book")
 	{
@@ -17,5 +31,5 @@ func Routes()  {
 		book.DELETE("/delete{id}", handler.DeleteBook)
 	}
 
-	r.Run(":9000")
+	r.Run(":" + port)
 }
