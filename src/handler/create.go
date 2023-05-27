42869451e4e69c8a9fc0cc3a4399ac9caa592c09
yusrililhm <yusrililham62@gmail.com>
2023-05-27 18:45:35 +0700

    update create.go

--- src/handler/create.go
+++ src/handler/create.go
@@ -1,12 +1,24 @@
 package handler
 
 import (
+	"os/exec"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/yusrililhm/go-bookshelf-api/src/model"
 )
 
 func AddBook(c *gin.Context)  {
 	var book model.Book
+
+	id, _ := exec.Command("uuidgen").Output()
+	t := time.Now()
+
+	insertedAt := t.Format(time.RFC1123)
+	updateAt := insertedAt
+
+	book = model.Book{Id: string(id), InsertedAt: insertedAt, UpdateAt: updateAt}
+
 	if err := c.BindJSON(&book);err != nil {
 		c.JSON(400, gin.H{
 			"status": "fail",
@@ -15,11 +27,11 @@ func AddBook(c *gin.Context)  {
 		return
 	} else {
 		model.Books = append(model.Books, book)
-		c.JSON(200, gin.H{
+		c.JSON(201, gin.H{
 			"status": "success",
 			"message": "Buku berhasil ditambahkan",
-			"book": map[string]any{
-				"id": book.Id,
+			"data": map[string]any{
+				"bookId": book.Id,
 			},
 		})
 	}
