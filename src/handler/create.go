7cbb3dafc8eade5b250c71ba1ef7a1310ce4943c
yusrililhm <yusrililham62@gmail.com>
2023-05-28 05:01:40 +0700

    add error respon

--- src/handler/create.go
+++ src/handler/create.go
@@ -11,20 +11,36 @@ import (
 func AddBook(c *gin.Context)  {
 	var book model.Book
 
+	// generate unique id
+
 	id, _ := exec.Command("uuidgen").Output()
 	t := time.Now()
 
 	insertedAt := t.Format(time.RFC1123)
 	updateAt := insertedAt
 
-	book = model.Book{Id: string(id), InsertedAt: insertedAt, UpdateAt: updateAt}
+	// finished = pageCount == readPage
 
-	if err := c.BindJSON(&book);err != nil {
-		c.JSON(400, gin.H{
+	finish := book.PageCount == book.ReadPage
+
+	book = model.Book{Id: string(id), Finished: finish, InsertedAt: insertedAt, UpdateAt: updateAt}
+
+	if err := c.BindJSON(&book); err != nil {
+		c.JSON(500, gin.H{
 			"status": "fail",
-			"message": "gagal menambahkan buku : " + err.Error(),
+			"message": "Buku gagal ditambahkan",
 		})
 		return
+	} else if book.Name == "" {
+		c.JSON(400, gin.H{
+			"status": "fail",
+      "message": "Gagal menambahkan buku. Mohon isi nama buku",
+		})
+	} else if book.ReadPage > book.PageCount {
+		c.JSON(400, gin.H{
+			"status": "fail",
+			"message": "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount",
+		})
 	} else {
 		model.Books = append(model.Books, book)
 		c.JSON(201, gin.H{
