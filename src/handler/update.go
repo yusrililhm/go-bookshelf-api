52fdbaeab91ac0acf41838925fefa9507947ea6a
yusrililhm <yusrililham62@gmail.com>
2023-05-30 07:29:35 +0700

    update update.go

--- src/handler/update.go
+++ src/handler/update.go
@@ -1,10 +1,18 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/yusrililhm/go-bookshelf-api/src/model"
+)
 
 func UpdateBook(c *gin.Context)  {
-	c.JSON(200, gin.H{
-		"status": "success",
-		"message": "Buku berhasil diupdate",
-	})
+	id := c.Param("id")
+	for _, v := range model.Books {
+		if v.Id == id {
+			c.JSON(200, gin.H{
+				"status": "success",
+				"message": "Buku berhasil diupdate",
+			})
+		}
+	}
 }
